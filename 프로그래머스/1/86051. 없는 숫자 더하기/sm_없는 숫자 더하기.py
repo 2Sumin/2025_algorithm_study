30f4f567282f3891c1972d9cdcda051762a897c5
Lee Sumin <102293299+2Sumin@users.noreply.github.com>
2025-04-02 00:38:43 +0900

    Update sm_없는 숫자 더하기.py

--- 프로그래머스/1/86051. 없는 숫자 더하기/sm_없는 숫자 더하기.py
+++ 프로그래머스/1/86051. 없는 숫자 더하기/sm_없는 숫자 더하기.py
@@ -1,7 +1,6 @@
 def solution(numbers):
-    ans = 0
-    numbers.sort()
-    for i in range(0,10):
-        if i not in numbers:
-             ans += i
-    return ans
+    # 차집합으로 없는 숫자 구하기
+    # range(10): [0, 1, 2, 3, 4, 5, 6, 7, 8, 9] - range 객체
+    # set(range(10)): {0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
+    missing_numbers = set(range(10)) - set(numbers)
+    return sum(missing_numbers)
