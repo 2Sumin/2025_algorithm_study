7d92d838c1abdd7481a113ba2d7e65a7e2fe438e
Lee Sumin <102293299+2Sumin@users.noreply.github.com>
2025-04-06 17:44:04 +0900

    Update sm_주식가격.py

--- 프로그래머스/2/42584. 주식가격/sm_주식가격.py
+++ 프로그래머스/2/42584. 주식가격/sm_주식가격.py
@@ -1,12 +1,14 @@
 def solution(prices):
-    ans = [0]*len(prices)
-    for i in range(len(prices)):
-        curr_price = prices[i]
-        for j in range(i+1, len(prices)):
-            if prices[j] < curr_price:
-                ans[i] = j-i
-                break
-            elif j == len(prices)-1:
-                ans[i] = j-i
-
+    ans = [0] * len(prices)
+    stack = []
+    for day, price in enumerate(prices):
+        # price(현재 가격)보다 높은 가격이 스택에 있으면 Pop
+        while stack and stack[-1][1] > price:
+            prev_day = stack.pop()[0] # 가격이 떨어진 시점의 인덱스
+            ans[prev_day] = day-prev_day # 떨어지기까지 걸린 시간
+        stack.append((day, price))
+    # 가격이 떨어지지 않은 주식
+    while stack:
+        prev_day = stack.pop()[0]
+        ans[prev_day] = len(prices)-1-prev_day
     return ans
